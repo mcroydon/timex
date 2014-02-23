07976c4fb2d30f595478f27139142ca9f3cf0512
Matt Croydon <mcroydon@gmail.com>
2014-02-23 08:29:40 -0600

    Testing: Updated Timex.Time zero check to use IsZero(), removed Timex.Status check since Timex.Ok is 0, added benchmark to get an idea of what Timex information looks like in a tight loop.

--- timex_test.go
+++ timex_test.go
@@ -3,6 +3,7 @@ package timex
 import (
 	"testing"
 	"time"
+	"github.com/rcrowley/go-metrics"
 )
 
 // Basic usage testing. If this fails, Timex probably won't work on your system.
@@ -11,11 +12,8 @@ func TestBasic(t *testing.T) {
 	tmx := Now()
 
 	// Make sure that the basic information is not the zero value.
-	if tmx.Time == time.Date(1, 1, 1, 1, 1, 1, 1, time.UTC) {
+	if tmx.Time.IsZero() {
 		t.Errorf("Timex.Time is nil.")
-	}
-	if tmx.Status == 0 {
-		t.Errorf("Timex.Status is 0.")
 	}
 	if tmx.StatusInfo() == "" {
 		t.Errorf("Timex.StatusInfo() is \"\".")
@@ -30,4 +28,26 @@ func TestBasic(t *testing.T) {
 	// Log current values for those running verbose tests.
 	t.Logf("Time: %v Status: %v Maxerror: %v Esterror: %v\n",
 		tmx.Time, tmx.StatusInfo(), tmx.Maxerror, tmx.Esterror)
+}
+
+func Benchmark(b *testing.B) {
+	// TODO: look at timedelta and clock error during benchmark runs.
+	s := metrics.NewUniformSample(100)
+	est := metrics.NewHistogram(s)
+	max := metrics.NewHistogram(s)
+	delta := metrics.NewHistogram(s)
+
+	var prev time.Time
+	for i := 0; i < b.N; i++ {
+		tmx := Now()
+		if prev.Year() != 1 {
+			delta.Update(int64(time.Since(prev)/time.Nanosecond))
+		}
+		est.Update(tmx.Esterror)
+		max.Update(tmx.Maxerror)
+		prev = tmx.Time
+	}
+	b.Logf("Estimated error min %v max %v mean %v", est.Min(), est.Max(), est.Mean())
+	b.Logf("Maximum error min %v max %v mean %v", max.Min(), max.Max(), max.Mean())
+	b.Logf("Time delta min %v max %v mean %v", delta.Min(), delta.Max(), delta.Mean())
 }
